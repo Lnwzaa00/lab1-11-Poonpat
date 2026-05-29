8a597a88bdcb41fd7b31e223d10046335a8f8ae6
Lnwzaa00 <Poonpat.meesuk@e-tech.ac.th>
2026-05-29 15:13:42 +0700

    Proflies

---
+++ lib/l1.dart
@@ -0,0 +1,11 @@
+void main()
+{
+  String fname = "ก้านคอ";
+  int age = 18;
+  int high = 250;
+  bool x = true;
+  double sc = 4.00;
+  String g = "กรุงธนบุรี";
+
+  print("ชื่อ:$fname \nอายุ:$age \nส่วนสูง:$high \nเป็นนักเรียนใช่หรือไม่:$x \nคะแนนเฉลี่ย:$sc \nจังหวัดที่อาศัย:$g");
+}
